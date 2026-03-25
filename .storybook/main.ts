c9a43ef11b5c4222479637bf2d41ce875509f353
Benjamin Saravia <elbenja@gmail.com>
2026-03-25 18:13:01 -0400

    feat: add Storybook docs addon to configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,7 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-vitest",
+    "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-themes",
     "@storybook/addon-mcp",
